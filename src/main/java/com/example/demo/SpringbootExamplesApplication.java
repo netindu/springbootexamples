06bf2b5f51d39769f64a6562da0afa9e725dbd63
netindu <netindu@gmail.com>
2021-02-22 01:24:31 -0500

    removed the authentication

--- src/main/java/com/example/demo/SpringbootExamplesApplication.java
+++ src/main/java/com/example/demo/SpringbootExamplesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.example.demo", "com.examples.demo.rest.api", "com.examples.demo.rest.security.basic"})
+@ComponentScan({"com.example.demo", "com.examples.demo.rest.api"})
 @SpringBootApplication
 public class SpringbootExamplesApplication {
 
