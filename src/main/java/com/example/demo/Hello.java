cb01f5d8f97867b598201849665e40f940d407e7
netindu <netindu@gmail.com>
2021-02-27 03:26:32 -0500

    Added swagger, port, etc.

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 	
-	@RequestMapping("/test")
+	@RequestMapping("/api/test")
 	public String greeting() {
 		
 		System.out.println("In method greeting in main package");
 		return "Welcome to Spring boot Application. Test Connection Successfull.";
 		
 	}
+	
 
 }
--- src/main/java/com/examples/demo/rest/api/SimpleHello.java
+++ src/main/java/com/examples/demo/rest/api/SimpleHello.java
@@ -2,6 +2,7 @@ package com.examples.demo.rest.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,20 +12,21 @@ public class SimpleHello {
 	public String sayHello() {
 		
 		System.out.println("In method sayHello");
-		return "Welcome to Rest API Services.";
+		return "Welcome to Rest API Services!";
 		
 	}
 	
-	@RequestMapping("/hello")
+	@RequestMapping(method = RequestMethod.GET, value = "/api/hello")
 	public String greeting() {
 		
 		System.out.println("In method greeting");
-		return "Hello World from Rest API Services";
+		return "Hello World from Rest API Services!";
 		
 	}
 	
-	//@GetMapping(path = "/", produces = "application/text")
-	@RequestMapping(path = "/hello1")
+	// Alternate ways to use the annotations.
+	/*
+	@RequestMapping(path = "/api/hello1", method = RequestMethod.GET)
 	public String greeting1() {
 		
 		System.out.println("In method greeting 1");
@@ -32,12 +34,14 @@ public class SimpleHello {
 		
 	}
 	
-	@GetMapping(path = "/hello2", produces = "application/text")
+	@GetMapping(path = "/api/hello2", produces = "application/text")
 	public String greeting2() {
 		
 		System.out.println("In method greeting 2");
 		return "Hello World 2 from Rest API Services";
 		
 	}
+	
+	*/
 
 }
